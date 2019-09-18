8c51e6999183bcca603ab9e75fb553793c905aa3
Akinbode Weezykon <weezykon@gmail.com>
2019-09-18 15:48:01 +0100

    Update db.inc.php

--- api/connect/db.inc.php
+++ api/connect/db.inc.php
@@ -1,5 +1,6 @@
 <?php
-$con = mysqli_connect("localhost","root","root","teamdominicians");
+// $con = mysqli_connect("localhost","root","root","teamdominicians");dominicans2019
+$con = mysqli_connect("sql112.byethost.com","b14_24497734","dominicans2019","teamdominicians");
 
 // Check connection
 if (mysqli_connect_errno())
